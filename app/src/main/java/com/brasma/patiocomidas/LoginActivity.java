9613d8d2c6b37c4b48072f00e6057faaa6a43392
Stiwar0098 <brayan98macas@gmail.com>
2021-05-07 03:08:44 -0500

    login 1.2.2 boton acceder google

--- app/src/main/java/com/brasma/patiocomidas/LoginActivity.java
+++ app/src/main/java/com/brasma/patiocomidas/LoginActivity.java
@@ -1,17 +1,72 @@
 package com.brasma.patiocomidas;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient googleApiClient;
+    private SignInButton btnAccedorGoogle;
+    private static final int SIGN_IN_CODE=77;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnAccedorGoogle=(SignInButton)findViewById(R.id.btnAccederGoogle_Login);
+        GoogleSignInOptions gso= new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        googleApiClient=new GoogleApiClient.Builder(this)
+        .enableAutoManage(this,this)
+        .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+        .build();
+        btnAccedorGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent,SIGN_IN_CODE);
+            }
+        });
+    }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
-        setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==SIGN_IN_CODE){
+            GoogleSignInResult result= Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSingInResult(result);
+        }
+    }
+
+    private void handleSingInResult(GoogleSignInResult result) {
+        if(result.isSuccess()){
+            abrirRegistro();
+        }else{
+            Toast.makeText(this,R.string.login_noLogin,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void abrirRegistro() {
+        Intent intent= new Intent(this, activity_registro.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
