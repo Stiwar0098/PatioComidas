a2d3f28e9189e9e45d4ef2aee64cb561ad69c485
Stiwar0098 <brayan98macas@gmail.com>
2021-05-07 20:33:12 -0500

    login 1.3 boton acceder google completo

--- app/src/main/java/com/brasma/patiocomidas/LoginActivity.java
+++ app/src/main/java/com/brasma/patiocomidas/LoginActivity.java
@@ -6,19 +6,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
 
-public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private GoogleApiClient googleApiClient;
+    GoogleSignInClient mGoogleSignInClient;
     private SignInButton btnAccedorGoogle;
     private static final int SIGN_IN_CODE=77;
     @Override
@@ -26,47 +33,74 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         btnAccedorGoogle=(SignInButton)findViewById(R.id.btnAccederGoogle_Login);
-        GoogleSignInOptions gso= new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
-        googleApiClient=new GoogleApiClient.Builder(this)
-        .enableAutoManage(this,this)
-        .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
-        .build();
-        btnAccedorGoogle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent= Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-                startActivityForResult(intent,SIGN_IN_CODE);
-            }
-        });
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        btnAccedorGoogle.setOnClickListener(this);
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==SIGN_IN_CODE){
-            GoogleSignInResult result= Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSingInResult(result);
-        }
-    }
-
-    private void handleSingInResult(GoogleSignInResult result) {
-        if(result.isSuccess()){
-            abrirRegistro();
+    private void updateUI(GoogleSignInAccount account) {
+        if(account==null){
+            Toast.makeText(this,"no hay usuario",Toast.LENGTH_SHORT).show();
         }else{
-            Toast.makeText(this,R.string.login_noLogin,Toast.LENGTH_SHORT).show();
+            abrirRegistro();
         }
     }
+
 
     private void abrirRegistro() {
         Intent intent= new Intent(this, activity_registro.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnAccederGoogle_Login:
+                signIn();
+                break;
+            // ...
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, SIGN_IN_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == SIGN_IN_CODE) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("ERRORR", "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
     }
 }
--- app/src/main/java/com/brasma/patiocomidas/activity_registro.java
+++ app/src/main/java/com/brasma/patiocomidas/activity_registro.java
@@ -3,12 +3,17 @@ package com.brasma.patiocomidas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class activity_registro extends AppCompatActivity {
 
+    Button btnCerrarSesion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        btnCerrarSesion= (Button)findViewById(R.id.btnCerrarSesion);
+        btnCerrarSesion.setOnClickListener(this);
     }
+
 }
