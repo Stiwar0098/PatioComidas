74af73dbf3f327802af98c63fa6d1721393e0bb0
Stiwar0098 <brayan98macas@gmail.com>
2021-05-08 01:24:22 -0500

    login 2.0 diseño registar

--- app/src/main/java/com/brasma/patiocomidas/activity_registro.java
+++ app/src/main/java/com/brasma/patiocomidas/activity_registro.java
@@ -1,19 +1,75 @@
 package com.brasma.patiocomidas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class activity_registro extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class activity_registro extends AppCompatActivity implements View.OnClickListener {
 
     Button btnCerrarSesion;
+    Button btnRemove;
+    GoogleSignInClient mGoogleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
         btnCerrarSesion= (Button)findViewById(R.id.btnCerrarSesion);
+        btnRemove= (Button)findViewById(R.id.btnRemove);
         btnCerrarSesion.setOnClickListener(this);
+        btnRemove.setOnClickListener(this);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnCerrarSesion:
+                signOut();
+                break;
+            case R.id.btnRemove:
+                revokeAccess();
+                break;
+            // ...
+        }
+    }
+    private void abrirLogin() {
+        Intent intent= new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+    private void revokeAccess() {
+        mGoogleSignInClient.revokeAccess()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(activity_registro.this,"removido",Toast.LENGTH_SHORT).show();
+                        abrirLogin();
+                    }
+                });
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(activity_registro.this,"Sesion cerrada",Toast.LENGTH_SHORT).show();
+                        abrirLogin();
+                    }
+                });
+    }
 }
